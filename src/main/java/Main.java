0c4056d9be7d7deb281a5440d3cd26f2f0ce2aee
James <james@Zipcoders-MacBook-Pro-9.local>
2022-11-09 21:33:32 -0500

    One More Down

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,34 @@
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
-        
+        Scanner Number = new Scanner(System.in);
+
+        int num1 = (int) (Math.random() * 100);
+        int guess = 5;
+        int tryMe;
+        System.out.println("Guess a number! Any number!");
+        for (int i = 0; i < guess; i++) {
+            tryMe = Number.nextInt();
+            if (num1 == tryMe) {
+                System.out.println("You got it dude!");
+            } else if (num1 > tryMe) {
+                System.out.println("Too small!");
+            } else if (num1 < tryMe) {
+                System.out.println("Too big!");
+
+            }
+
+            }//if (i == guess)
+        {
+            System.out.println("Better luck next time!");
+            System.out.println("The number was " + num1);
+
+        }
     }
 }
